2cc5c4fcd4fb236d0daaec540ba4abf3bd96fa24
joseflickr1 <joseflickr1@yahoo.com>
2018-06-03 08:08:09 +0200

    style override input

---
+++ src/assets/jss/material-fotohjelp-react.tsx
@@ -0,0 +1,16 @@
+const primaryColor = '#00AADBD';
+const primaryLighterColor = '#00BF9A';
+const primaryDarkerColor = '#008975';
+const secondaryColor = '#E91E63';
+
+const primaryContrastText = 'white';
+const secondaryContrastText = 'black';
+
+export {
+    primaryColor,
+    primaryLighterColor,
+    primaryDarkerColor,
+    secondaryColor,
+    primaryContrastText,
+    secondaryContrastText
+};
---
+++ src/components/common/DecoratedTextInput.tsx
@@ -0,0 +1,60 @@
+//tslint:disable
+import * as React from 'react';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
+
+import {withStyles} from '@material-ui/core/styles';
+import {primaryContrastText, primaryLighterColor} from "../../assets/jss/material-fotohjelp-react";
+
+/*
+* How to use
+*
+
+import DecoratedTextInput from '../common/DecoratedTextInput';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+* <DecoratedTextInput
+    id="Navn"
+    label="Navn"
+    startAdornment={
+        <InputAdornment position="start" >
+            <AccountCircle/>
+        </InputAdornment>
+    }
+
+/>
+*
+* */
+
+const decorate = withStyles(({palette, spacing}) => ({
+    root: {
+        backgroundColor: primaryLighterColor,
+        color: primaryContrastText,
+        padding: '5px 10px',
+        borderRadius: '5px'
+    },
+    underline: {
+        "&:hover:not($disabled):before,&:before": {
+            borderBottom: 'none !important'
+        },
+        "&:after": {
+            borderBottom: 'none !important'
+        }
+    }
+}));
+
+const DecoratedTextInput = decorate<any>(({id, label, inputProps, startAdornment, classes}) => (
+    <FormControl>
+        <Input
+            placeholder={label}
+            classes={classes}
+            inputProps={{
+                'aria-label': label,
+            }}
+            startAdornment={startAdornment}
+        />
+    </FormControl>
+));
+
+export default DecoratedTextInput;
